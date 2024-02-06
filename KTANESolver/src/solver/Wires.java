8e92101567fed0e4b7465e092c46508c7e0a2fa8
Arthur <arthurvininunes@gmail.com>
2024-02-06 16:13:40 -0300

    codigo em ingles

--- KTANESolver/src/solver/Wires.java
+++ KTANESolver/src/solver/Wires.java
@@ -8,19 +8,19 @@ public class Wires implements Module {
 	public void setWires(int n) {
 		for (int i = 0; i < n; i++) {
 			System.out.println("Digite a cor do fio " + (i + 1) + ":");
-			String cor = Main.LerEntrada();
+			String color = Main.LerEntrada();
 
 			// Count the number of wires of each color
-			if (wires.containsKey(cor)) {
-				int count = wires.get(cor);
-				wires.put(cor, count + 1);
+			if (wires.containsKey(color)) {
+				int count = wires.get(color);
+				wires.put(color, count + 1);
 			} else {
-				wires.put(cor, 1);
+				wires.put(color, 1);
 			}
 
 			// Save the last wire
 			if (i == n - 1) {
-				lastWire = cor;
+				lastWire = color;
 			}
 		}
 	}
@@ -38,16 +38,16 @@ public class Wires implements Module {
 			// Check the colors of the wires
 			for (int i = 0; i < n; i++) {
 				System.out.println("Digite a cor do fio " + (i + 1) + ":");
-				String cor = Main.LerEntrada();
+				String color = Main.LerEntrada();
 
-				if (cor.equals("vermelho")) {
-				} else if (cor.equals("azul")) {
-				} else if (cor.equals("branco")) {
+				if (color.equals("vermelho")) {
+				} else if (color.equals("azul")) {
+				} else if (color.equals("branco")) {
 				}
 			}
 
 			// Wire cutting logic
-			if (!hasRedWire) {
+			if (!wires.containsKey("vermelho")) {
 				System.out.println("Corte o segundo fio.");
 			} else if (lastWire.equals("branco")) {
 				System.out.println("Corte o último fio.");
@@ -57,8 +57,11 @@ public class Wires implements Module {
 				System.out.println("Corte o último fio.");
 			}
 		} else if (n == 4) {
+			//TODO: Wire cutting logic
 		} else if (n == 5) {
+			//TODO: Wire cutting logic
 		} else if (n == 6) {
+			//TODO: Wire cutting logic
 		}
 	}
 }
