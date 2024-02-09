4d769b1976d3a6ee2a058225d81e9ae5decb1995
Arthur <arthurvininunes@gmail.com>
2024-02-08 23:56:08 -0300

    Genius Done!

--- KTANESolver/src/solver/Genius.java
+++ KTANESolver/src/solver/Genius.java
@@ -1,16 +1,45 @@
 package solver;
 
 public class Genius implements Module {
+	// Variables to store the bomb indicators
+	private boolean hasVowel;
+	private int errors;
+	// Constants to store the colors
+	private static final String[][] colorVowel = { { "azul", "vermelho", "amarelo", "verde" },
+			{ "amarelo", "verde", "azul", "vermelho" },
+			{ "verde", "amarelo", "vermelho", "azul" } };
+	private static final String[][] colorNoVowel = { { "azul", "amarelo", "verde", "vermelho" },
+			{ "vermelho", "azul", "amarelo", "verde" },
+			{ "amarelo", "verde", "azul", "vermelho" } };
+	private String[] pattern;
+	private String sequence = "Aperte: ";
+	private String input;
 
 	@Override
 	public void setIndicators(Indicators indicators) {
-		// TODO Auto-generated method stub
-		
+		hasVowel = indicators.hasVowel();
 	}
 
 	@Override
 	public void solve() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Digite o numero de erros: ");
+		errors = Integer.parseInt(System.console().readLine());
+		pattern = hasVowel ? colorVowel[errors] : colorNoVowel[errors];
+		System.out.println("Digite a cor:");
+		input = Main.LerEntrada();
+		while (input != "fim") {
+			if (input.equals("vermelho")) {
+				sequence += pattern[0];
+			} else if (input.equals("azul")) {
+				sequence += pattern[1];
+			} else if (input.equals("verde")) {
+				sequence += pattern[2];
+			} else if (input.equals("amarelo")) {
+				sequence += pattern[3];
+			}
+			System.out.println(sequence);
+			System.out.println("Digite a nova cor ou 'fim' para concluir:");
+			input = Main.LerEntrada();
+		}
 	}
 }
